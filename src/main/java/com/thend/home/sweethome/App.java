1980528d00e9b5caa724da5e517006c3a3896019
thend <carrollwk@163.com>
2014-01-02 17:12:06 +0800

    add src

---
+++ src/main/java/com/thend/home/sweethome/App.java
@@ -0,0 +1,13 @@
+package com.thend.home.sweethome;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
